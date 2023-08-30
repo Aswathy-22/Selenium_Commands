d0eb506f07e4ea9039a00586d5a2e17cf9971d26
Aswathy-22 <138908063+Aswathy-22@users.noreply.github.com>
2023-08-30 10:35:35 +0530

    selenium commands

--- src/test/java/org/obs/homework/RadioButtonsDemoMenu.java
+++ src/test/java/org/obs/homework/RadioButtonsDemoMenu.java
@@ -8,43 +8,44 @@ public class RadioButtonsDemoMenu extends Baseform{
     @Test
     public void radioButtonMenu() {
         driver.get("https://selenium.obsqurazone.com/radio-button-demo.php");
-        List<WebElement> genders = driver.findElements(By.xpath("//input[@name='inlineRadioOptions']/following-sibling::label"));
-        String genderToSelect="Female";
-        selectGender(genderToSelect,genders);
-    }
-    public void selectGender(String gender, List<WebElement> genders) {
-        for (int i = 0; i < genders.size(); i++) {
-            WebElement gen = genders.get(i);
-            String genderValue = gen.getText();
-            if (genderValue.equals(gender)) {
-                gen.click();
-            }
-        }
-        WebElement showSelectedValueButton= driver.findElement(By.xpath("//button[@id='button-one']"));
+        List<WebElement> genderList = driver.findElements(By.xpath("//input[@name='inlineRadioOptions']//following-sibling::label"));
+        genderSelect("Female", genderList);
+        WebElement showSelectedValueButton = driver.findElement(By.id("button-one"));
         showSelectedValueButton.click();
+        boolean isDisplayed= showSelectedValueButton.isDisplayed();
+        Assert.assertTrue(isDisplayed,"Button not displayed");
+        List<WebElement> patientGenderList = driver.findElements(By.xpath("//input[@name='student-gender']//following-sibling::label"));
+        patientGenderSelect("Female", patientGenderList);
+        List<WebElement> patientAgeGroupList = driver.findElements(By.xpath("//input[@name='student-age']/following-sibling::label"));
+        patientAgeGroup("19 t0 44", patientAgeGroupList);
+        WebElement getResultsButton = driver.findElement(By.id("button-two"));
+        getResultsButton.click();
     }
-    @Test
-    public void groupRadioButton(){
-        driver.get("https://selenium.obsqurazone.com/radio-button-demo.php");
-        List<WebElement>patientGender=driver.findElements(By.xpath("//input[@name='student-gender']"));
-        List<WebElement>patientAgegroup=driver.findElements(By.xpath("//input[@name='student-age']"));
-        selectGroupRadioButton("Female","45 to 60",patientGender,patientAgegroup);
-        WebElement selectGen= driver.findElement(By.xpath("//input[@id='inlineRadio21']"));
-        String actualGender= selectGen.getText();
-        Assert.assertTrue(actualGender.contains("Female"),"Selected gender should be Female");
-        WebElement selectAge= driver.findElement(By.xpath("//input[@id='inlineRadio24']"));
-        String actualAgeSelected=selectAge.getText();
-    }
-    public void selectGroupRadioButton(String gender, String ageGroup, List<WebElement> patientGender, List<WebElement> patientAgegroup) {
-       selectRadioButton(gender,patientGender);
-       selectRadioButton(ageGroup,patientAgegroup);
-    }
-    public void selectRadioButton(String menu, List<WebElement> radiobutton) {
-        for (WebElement Button: radiobutton) {
-            String actualmenu=Button.getText();
-            if(actualmenu.equals(menu)){
-                Button.click();
+    public void genderSelect(String gender, List<WebElement> genderList) {
+        for (int i = 0; i < genderList.size(); i++) {
+            WebElement genderElement = genderList.get(i);
+            String genderText = genderElement.getText();
+            if (genderText.equals(gender)) {
+                genderElement.click();
             }
         }
     }
-}
+    public void patientGenderSelect(String patenintGender, List<WebElement> patientGenderList) {
+        for (int j = 0; j < patientGenderList.size(); j++) {
+            WebElement genderElement = patientGenderList.get(j);
+            String genderText = genderElement.getText();
+            if (genderText.equals(patenintGender)) {
+                genderElement.click();
+            }
+        }
+    }
+        public void patientAgeGroup(String age, List < WebElement > patientAgeGroupList){
+            for (int k = 0; k < patientAgeGroupList.size(); k++) {
+                WebElement ageElement = patientAgeGroupList.get(k);
+                String ageElementText = ageElement.getText();
+                if (ageElementText.equals(age)) {
+                    ageElement.click();
+                }
+            }
+        }
+    }
